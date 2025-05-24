42eef83a6105d43ff7cb45d54caf571368ea5e34
RosRaven <sao.dor.oati@gmail.com>
2025-05-24 13:22:14 +0300

    chore: register pages and blog apps in INSTALLED_APPS

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # наши приложения
+    'pages.apps.PagesConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
