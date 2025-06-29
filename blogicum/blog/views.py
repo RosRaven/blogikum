ef4da0117b1d930aafd2e7e4d4c567c22bdbcce7
RosRaven <sao.dor.oati@gmail.com>
2025-06-29 14:02:20 +0300

    sprint-3: moved the magic variable POSTS_ON_MAIN responsible for the number of posts on the main page to constants

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
+from .constants import POSTS_ON_MAIN
 from .models import Post, Category
 
 
@@ -9,7 +10,7 @@ def index(request):
              .filter(is_published=True,
                      pub_date__lte=timezone.now(),
                      category__is_published=True)
-             .order_by("-pub_date")[:5]
+             .order_by("-pub_date")[:POSTS_ON_MAIN]
              )
     return render(request, "blog/index.html", {"post_list": post_list})
 
